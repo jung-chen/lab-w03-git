7e6a60fa56b26bafc2d6fa3cbd632be1c12abb21
Jung Chen <jchen378@ucmerced.edu>
2020-09-10 19:23:17 -0700

    problem 1 step 3

--- lab.R
+++ lab.R
@@ -36,6 +36,7 @@ opts_chunk$set(out.width='60%', echo=FALSE, fig.align='center')
 #' 3. Edit `lab.R` again, making another round of changes.  Add and commit the changes to your working machine. In the commit message, put something like "problem 1 step 3." 
 #' 4. Push the repo back up to GitHub.  
 #' 5. Step 2
+#' 6. Step 3
 
 
 #' # Problem 2 #
